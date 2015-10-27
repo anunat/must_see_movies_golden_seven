254ac522f44e3a0e27c07bfb50c411a01ac791c8
anunat <anusha.hemanth@gmail.com>
2015-10-26 22:43:29 -0500

    10_26_2

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,11 +1,11 @@
-class DirectorsController < ApplicationController
+class ActorsController < ApplicationController
 
   def index
-    @list_of_directors = Director.all
+    @list_of_actors = Actor.all
   end
 
   def show
-    @director = Director.find_by({:id => params[:id]})
+    @actor = Actor.find_by({:id => params[:id]})
   end
 
   def new_form
@@ -14,43 +14,43 @@ class DirectorsController < ApplicationController
 
   def create_row
 
-    p = Director.new
+    p = Actor.new
     p.name = params[:the_name]
     p.dob = params[:the_dob]
     p.bio = params[:the_bio]
     p.image_url = params[:the_image_url]
     p.save
 
-    redirect_to("/directors")
+    redirect_to("/actors")
 
   end
 
   def destroy
 
-    p= Director.find_by({:id => params[:id]})
+    p= Actor.find_by({:id => params[:id]})
 
     p.destroy
 
-    redirect_to("/directors")
+    redirect_to("/actors")
 
   end
 
   def edit_form
 
-    @director = Director.find_by({:id => params[:id]})
+    @actor = Actor.find_by({:id => params[:id]})
 
   end
 
   def update_row
 
-    p = Director.find_by({:id => params[:id]})
+    p = Actor.find_by({:id => params[:id]})
     p.name = params[:the_name]
     p.bio = params[:the_bio]
     p.image_url = params[:the_image_url]
     p.dob = params[:the_dob]
     p.save
 
-    redirect_to("/directors")
+    redirect_to("/actors")
 
   end
 
